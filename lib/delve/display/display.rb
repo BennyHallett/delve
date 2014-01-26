9db783fb19fe31a9fa2b2e8fded8440751d889d1
BennyHallett <benny.hallett@gmail.com>
2014-01-26 16:16:43 +1100

    Setting options correctly

--- lib/delve/display/display.rb
+++ lib/delve/display/display.rb
@@ -2,7 +2,9 @@ class Display
 
   def initialize(renderer, options=Hash.new)
     raise 'Cannot initialize display with no renderer' unless renderer
-    
+   
+    @renderer = renderer
+ 
     default_options = {
       :width        => 80,
       :height       => 24,
@@ -10,6 +12,7 @@ class Display
       :foreground   => :white
     }
 
+    options.keys.each { |k| default_options[k] = options[k] }
     @options = default_options
   end
 
--- test/display/display_test.rb
+++ test/display/display_test.rb
@@ -25,4 +25,24 @@ class DisplayTests < MiniTest::Test
     assert_equal :black, opts[:background]
   end
 
+  def test_partially_setting_options_keeps_changes_and_defults
+    @display = Display.new(@renderer, { :height => 36, :width => 120 })
+    opts = @display.options
+
+    assert_equal 120, opts[:width]
+    assert_equal 36, opts[:height]
+    assert_equal :white, opts[:foreground]
+    assert_equal :black, opts[:background]
+  end
+
+  def test_completely_define_options
+    @display = Display.new(@renderer, { :height => 36, :width => 120, :background => :gray, :foreground => :yellow })
+    opts = @display.options
+
+    assert_equal 120, opts[:width]
+    assert_equal 36, opts[:height]
+    assert_equal :yellow, opts[:foreground]
+    assert_equal :gray, opts[:background]
+  end
+  
 end
