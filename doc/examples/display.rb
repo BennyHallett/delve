35a62223f2df55f328358c4b517cf48adbc1603a
BennyHallett <benny.hallett@gmail.com>
2014-01-30 08:06:35 +1100

    Added user input into the example

--- doc/examples/display.rb
+++ doc/examples/display.rb
@@ -7,10 +7,11 @@ display.draw(2, 2, '@')
 display.draw(4, 6, 'x')
 display.render
 
-sleep 2
+x = display.wait_for_input
 
 display.draw(5, 1, 'y')
 display.draw(3, 3, 'p')
+display.draw(0, 0, x)
 display.render
 
 sleep 2
