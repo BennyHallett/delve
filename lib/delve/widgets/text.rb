db0237538f227e8f1e343c81ddc584df131e8a08
BennyHallett <benny.hallett@gmail.com>
2014-01-29 21:57:09 +1100

    Checking pre conditions on text widget

--- lib/delve/widgets/text.rb
+++ lib/delve/widgets/text.rb
@@ -1,5 +1,7 @@
 class TextWidget
   def initialize(x, y, text)
-    
+    raise 'Cannot initialize text widget when x is nil' unless x
+    raise 'Cannot initialize text widget when y is nil' unless y
+    raise 'Cannot initialize text widget when text is nil' unless text
   end
 end
