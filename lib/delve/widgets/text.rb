0c79fa69c886b1922c46887569343472417bc296
BennyHallett <benny.hallett@gmail.com>
2014-01-30 07:49:57 +1100

    Drawing the text widget

--- lib/delve/widgets/text.rb
+++ lib/delve/widgets/text.rb
@@ -3,5 +3,19 @@ class TextWidget
     raise 'Cannot initialize text widget when x is nil' unless x
     raise 'Cannot initialize text widget when y is nil' unless y
     raise 'Cannot initialize text widget when text is nil' unless text
+
+    @x = x
+    @y = y
+    @text = text
+  end
+
+  def draw(display)
+    raise 'Cannot draw text when display is nil' unless display
+
+    x = @x
+    @text.each_char do |c|
+      display.draw x, @y, c
+      x += 1
+    end
   end
 end
--- test/widgets/text_test.rb
+++ test/widgets/text_test.rb
@@ -4,9 +4,6 @@ require 'delve/widgets/text'
 
 class TextWidgetTest < Minitest::Test
 
-  def setup
-  end
-
   def test_initialising_without_x_position_raises_error
     assert_raises RuntimeError do
       TextWidget.new nil, 5, 'This is some text'
@@ -23,5 +20,27 @@ class TextWidgetTest < Minitest::Test
     assert_raises RuntimeError do
       TextWidget.new 5, 5, nil
     end
+  end
+
+  def test_render_fails_if_no_display_is_given
+    widget = TextWidget.new 2, 2, 'Text'
+    assert_raises RuntimeError do
+      widget.draw nil
+    end
+  end
+
+  def test_render
+    display = mock('onject')
+    widget = TextWidget.new 1, 1, 'Some text'
+    display.expects(:draw).with(1, 1, 'S')
+    display.expects(:draw).with(2, 1, 'o')
+    display.expects(:draw).with(3, 1, 'm')
+    display.expects(:draw).with(4, 1, 'e')
+    display.expects(:draw).with(5, 1, ' ')
+    display.expects(:draw).with(6, 1, 't')
+    display.expects(:draw).with(7, 1, 'e')
+    display.expects(:draw).with(8, 1, 'x')
+    display.expects(:draw).with(9, 1, 't')
+    widget.draw(display)
   end
 end
