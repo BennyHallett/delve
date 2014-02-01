9c4a4a27176d5a8814f1dc37bc73e67009a4d9b6
BennyHallett <benny.hallett@gmail.com>
2014-02-01 15:39:44 +1100

    Updated roadmap. Updated display example to include console size

--- ROADMAP.md
+++ ROADMAP.md
@@ -7,5 +7,5 @@ Roadmap
 * Coloured output
 * Cursor off screen after render [DONE]
 * Update requires means of input [DONE]
-* Display can determine size of console
+* Display can determine size of console [DONE]
 * Noise takes step size for fine-grained vs coarse-grained (4 or 5 options?)
--- doc/examples/display.rb
+++ doc/examples/display.rb
@@ -7,15 +7,17 @@ require 'delve/widgets/text'
 title = TextWidget.new 0, 0, 'Press a character key to continue'
 display = Display.new CursesRenderer.new
 input = Input.new CursesInput.new
+size = TextWidget.new 0, 1, "The console is #{display.width}x#{display.height}."
 
 title.draw display
+size.draw display
 display.draw(2, 2, '@')
 display.draw(4, 6, 'x')
 display.render
 
 x = input.wait_for_input
 
-display.draw(5, 1, 'y')
+display.draw(5, 4, 'y')
 display.draw(3, 3, 'p')
 display.draw(0, 0, x)
 display.render
