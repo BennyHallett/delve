1e6a37e258492a05eafd13e3c9902d85d29f82e5
BennyHallett <benny.hallett@gmail.com>
2014-02-04 08:38:06 +1100

    Added tests around movement style

--- lib/delve/component/movement.rb
+++ lib/delve/component/movement.rb
@@ -1,7 +1,12 @@
 class MovementComponent
+
+  @@styles = [:fourway, :eightway]
+  @@diagonal_error = 'Style is not sufficient to move diagonally'
+
   def initialize(parent, movement_style=:fourway)
     raise 'Cannot create movement component when parent is nil' unless parent
     @style = movement_style || :fourway
+    raise 'Cannot create movement component with unknown style' unless @@styles.include?(@style)
   end
 
   def id
@@ -11,4 +16,29 @@ class MovementComponent
   def style
     @style
   end
+
+  def north_west
+    raise @@diagonal_error unless diagonal_movement_is_valid
+  end
+
+  def north_east
+    raise @@diagonal_error unless diagonal_movement_is_valid
+
+  end
+
+  def south_west
+    raise @@diagonal_error unless diagonal_movement_is_valid
+
+  end
+
+  def south_east
+    raise @@diagonal_error unless diagonal_movement_is_valid
+
+  end
+
+  private
+  def diagonal_movement_is_valid
+    @style == :eightway
+  end
+
 end
--- test/component/movement_test.rb
+++ test/component/movement_test.rb
@@ -7,10 +7,9 @@ class MovementComponentTest < Minitest::Test
 
   def setup
     @parent = mock('object')
+    @fourway = MovementComponent.new @parent
   end
 
-  # test init 4 way vs 8 way
-  # test invalid movement style
   # test all diag movement on 4 way
   # test all movement styles
 
@@ -28,6 +27,36 @@ class MovementComponentTest < Minitest::Test
   def test_initialize_with_eight_way_movement
     move = MovementComponent.new @parent, :eightway
     assert_equal :eightway, move.style
+  end
+
+  def test_initialize_with_invalid_movement_style
+    assert_raises RuntimeError do
+      MovementComponent.new @parent, :unknown
+    end
+  end
+
+  def test_moving_north_west_in_fourway_fails
+    assert_raises RuntimeError do
+      @fourway.north_west
+    end
+  end
+
+  def test_moving_north_east_in_fourway_fails
+    assert_raises RuntimeError do
+      @fourway.north_east
+    end
+  end
+
+  def test_moving_south_west_in_fourway_fails
+    assert_raises RuntimeError do
+      @fourway.south_west
+    end
+  end
+
+  def test_moving_south_east_in_fourway_fails
+    assert_raises RuntimeError do
+      @fourway.south_east
+    end
   end
 
 end
