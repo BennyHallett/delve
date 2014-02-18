033607e5efd6c7e3091f8969e8fee1c5843da0e3
BennyHallett <benny.hallett@gmail.com>
2014-02-18 17:47:15 +1100

    Choosing generator and renderer

--- lib/delve.rb
+++ lib/delve.rb
@@ -2,8 +2,30 @@ class Delve
 
   def initialize(name)
     raise 'Cannot create a new roguelike without a name' unless name
-
     @name = name
+
+    @renderers = [:curses]
+    @generators = [:noise, :rogue, :cellular]
+    @renderer = :curses
+    @generator = :rogue
+  end
+
+  def use_renderer(renderer)
+    raise "Cannot use unknown renderer: #{renderer}" unless @renderers.include? renderer
+    @renderer = renderer
+  end
+
+  def use_generator(generator)
+    raise "Cannot use unknown generator: #{generator}" unless @generators.include? generator
+    @generator = generator
+  end
+
+  def renderer
+    @renderer
+  end
+
+  def generator
+    @generator
   end
 
 end
--- test/delve_test.rb
+++ test/delve_test.rb
@@ -6,12 +6,33 @@ class DelveTest < Minitest::Test
 
   def setup
     @name = 'Dungeon Crawler'
+    @delve = Delve.new @name
   end
 
   def test_initialize_without_name_fails
     assert_raises RuntimeError do
       Delve.new nil
     end
+  end
+
+  def test_set_unknown_renderer_fails
+    assert_raises RuntimeError do
+      @delve.use_renderer :train
+    end
+  end
+
+  def test_default_renderer_is_curses
+    assert_equal :curses, @delve.renderer
+  end
+
+  def test_set_unknown_generator_fails
+    assert_raises RuntimeError do
+      @delve.use_generator :window
+    end
+  end
+
+  def test_default_generator_is_rogue
+    assert_equal :rogue, @delve.generator
   end
 
 end
